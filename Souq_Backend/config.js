74f2977627006490cb55e5480129208f35dc077b
davik <googolrj@gmail.com>
2020-08-30 19:44:18 +0530

    jawsdb values

--- Souq_Backend/config.js
+++ Souq_Backend/config.js
@@ -2,10 +2,10 @@ const mysql = require('mysql');
 
 const connectionPool = mysql.createPool({
     connectionLimit: 10,
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'souq',
+    host: 'nwhazdrp7hdpd4a4.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    user: 'fdhmtiheauqwm0fl',
+    password: 'mx1qevesfb4jn03p',
+    database: 'jjdhl3nxaus3ojj2',
 })
 
 module.exports = connectionPool;
